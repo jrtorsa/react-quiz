7f929cdc5d1f8c9fe55c6b9deaee58b596e4d9a2
Raul T <jr.torsa@gmail.com>
2020-07-17 22:13:57 -0600

    added api endpoints and types

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
+import { fetchQuizQuestions } from "./API";
 // Components
-import QuestionCard from './components/QeustionCard'
+import QuestionCard from "./components/QeustionCard";
+// Types
+import { Difficulty } from "./API";
 
 const TOTAL_QUESTIONS = 10;
 
 const App = () => {
-  const [loading, setloading] = useState(falser);
+  const [loading, setloading] = useState(false);
   const [questions, setQuestions] = useState([]);
   const [number, setNumber] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
   const [score, setScore] = useState(0);
-  const [gameOver. setGameOver] = useState(true);
+  const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
 
   const startTrivia = async () => {};
 
@@ -19,23 +24,26 @@ const App = () => {
   const nextQuestion = () => {};
 
   return (
-  <div className="App">
-    <h1>REACT QUIZ</h1>
-    <button classNAme="start" onClick={startTrivia}>Start</button>
-    <p className="score">Score: </p>
-    <p>Loading Questions ...</p>
-    <QuestionCard
+    <div className="App">
+      <h1>REACT QUIZ</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className="score">Score: </p>
+      <p>Loading Questions ...</p>
+      {/* <QuestionCard
       questionNr={number + 1}
       totalQuestions={TOTAL_QUESTIONS}
       question={questions[number].question}
       answers={questions[number].answers}
       userAnswer={userAnswers ? userAnswers[number] : undefined}
       callback={checkAnswer}
-    />
-    <button className="next" onClick={nextQuestion}>
-      Next Question
-    </button>
-  </div>
+    /> */}
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  );
 };
 
 export default App;
