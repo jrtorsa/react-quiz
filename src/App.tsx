183506c7a1cc26138fba401ba35dc0b77e51228c
Raul T <jr.torsa@gmail.com>
2020-07-17 23:57:05 -0600

    completed logic, and refactor in questioncard.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import QuestionCard from "./components/QeustionCard";
 // Types
 import { QuestionState, Difficulty } from "./API";
 
-type AnswerObject = {
-  qustion: string;
+export type AnswerObject = {
+  question: string;
   answer: string;
   correct: boolean;
   correctAnswer: string;
@@ -38,9 +38,33 @@ const App = () => {
     setLoading(false);
   };
 
-  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!gameOver) {
+      //user answers
+      const answer = e.currentTarget.value;
+      //check answer against correct answer
+      const correct = questions[number].correct_answer === answer;
+      if (correct) setScore((prev) => prev + 1);
+      //save answer in the array for user answers
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswers((prev) => [...prev, answerObject]);
+    }
+  };
 
-  const nextQuestion = () => {};
+  const nextQuestion = () => {
+    //move on to the next question if not the last question
+    const nextQuestion = number + 1;
+    if (nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(nextQuestion);
+    }
+  };
 
   return (
     <div className="App">
